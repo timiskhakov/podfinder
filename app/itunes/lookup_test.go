862f16079bd8779801e88914a5406d8e77633575
Timur Iskhakov <timiskhakov@gmail.com>
2022-07-20 12:42:56 +0300

    Fix TestLookup

--- app/itunes/lookup_test.go
+++ app/itunes/lookup_test.go
@@ -22,12 +22,13 @@ func (s *StoreSuite) TestLookup() {
 
 	s.NoError(err)
 	s.Equal(&PodcastDetail{
-		Id:      "811377230",
-		Artist:  "CGP Grey & Brady Haran",
-		Name:    "Hello Internet",
-		Image:   "https://is5-ssl.mzstatic.com/image/thumb/Podcasts6/v4/19/33/fe/1933fe85-cd86-2191-8187-d725ca7359bf/mza_8038397602264410223.png/600x600bb.jpg",
-		Url:     "https://podcasts.apple.com/us/podcast/hello-internet/id811377230?uo=4",
-		FeedUrl: "http://www.hellointernet.fm/podcast?format=rss",
-		Genres:  []string{"Education", "Podcasts"},
+		Id:           "811377230",
+		Artist:       "CGP Grey & Brady Haran",
+		Name:         "Hello Internet",
+		Image:        "https://is5-ssl.mzstatic.com/image/thumb/Podcasts6/v4/19/33/fe/1933fe85-cd86-2191-8187-d725ca7359bf/mza_8038397602264410223.png/600x600bb.jpg",
+		EpisodeCount: 100,
+		Url:          "https://podcasts.apple.com/us/podcast/hello-internet/id811377230?uo=4",
+		FeedUrl:      "http://www.hellointernet.fm/podcast?format=rss",
+		Genres:       []string{"Education", "Podcasts"},
 	}, pd)
 }
