fa8d4ec71505730d4d2f97aa4b62b6953b4845f6
Timur Iskhakov <timiskhakov@gmail.com>
2023-01-24 13:02:10 +0200

    App refactoring

--- app/app.go
+++ app/app.go
@@ -6,7 +6,10 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 )
+
+const errorMessage = "Internal server error"
 
 type App struct {
 	mux   http.Handler
@@ -25,21 +28,7 @@ func NewApp(store Store) *App {
 
 	mux := http.NewServeMux()
 	mux.Handle("/www/", http.StripPrefix("/www/", http.FileServer(http.Dir("./www/"))))
-
-	mux.HandleFunc("/", a.handleHome())
-	mux.HandleFunc("/search", a.handleSearch())
-
-	a.mux = mux
-
-	return a
-}
-
-func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	a.mux.ServeHTTP(w, r)
-}
-
-func (a *App) handleHome() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
 			a.handleHomePost(w, r)
 			return
@@ -51,22 +40,33 @@ func (a *App) handleHome() http.HandlerFunc {
 		}
 
 		a.handleHomeGet(w, r)
-	}
+	})
+	mux.HandleFunc("/search", a.handleSearch())
+
+	a.mux = mux
+
+	return a
+}
+
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.mux.ServeHTTP(w, r)
 }
 
 func (a *App) handleHomeGet(w http.ResponseWriter, r *http.Request) {
 	podcasts, err := a.store.Top(region(r))
 	if err != nil {
-		render(w, r, nil, err, "./templates/base.html", "./templates/error.html")
+		log.Println(err)
+		render(w, r, nil, "./templates/error.html")
 		return
 	}
 
-	render(w, r, podcasts, nil, "./templates/base.html", "./templates/home.html")
+	render(w, r, podcasts, "./templates/home.html")
 }
 
 func (a *App) handleHomePost(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
-		render(w, r, nil, err, "./templates/base.html", "./templates/error.html")
+		log.Println(err)
+		render(w, r, nil, "./templates/error.html")
 		return
 	}
 
@@ -80,30 +80,32 @@ func (a *App) handleHomePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *App) handleSearch() http.HandlerFunc {
-	type queryAndPodcasts struct {
+	type response struct {
 		Query    string
 		Podcasts []*itunes.Podcast
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := r.ParseForm(); err != nil {
-			render(w, r, nil, err, "./templates/base.html", "./templates/error.html")
+			log.Println(err)
+			render(w, r, nil, "./templates/error.html")
 			return
 		}
 
 		query := r.Form.Get("query")
 		podcasts, err := a.store.Search(region(r), query)
 		if err != nil {
-			render(w, r, nil, err, "./templates/base.html", "./templates/error.html")
+			log.Println(err)
+			render(w, r, nil, "./templates/error.html")
 			return
 		}
 
-		render(w, r, queryAndPodcasts{query, podcasts}, nil, "./templates/base.html", "./templates/results.html")
+		render(w, r, response{query, podcasts}, "./templates/results.html")
 	}
 }
 
 func (a *App) handlePodcast(w http.ResponseWriter, r *http.Request, id string) {
-	type podcastAndReviews struct {
+	type response struct {
 		Podcast *itunes.PodcastDetail
 		Reviews []*itunes.Review
 	}
@@ -111,39 +113,48 @@ func (a *App) handlePodcast(w http.ResponseWriter, r *http.Request, id string) {
 	// TODO(timiskhakov): Obtain podcast and reviews at the same time
 	podcast, err := a.store.Lookup(id)
 	if err != nil {
-		render(w, r, nil, nil, "./templates/base.html", "./templates/404.html")
+		log.Println(err)
+		render(w, r, nil, "./templates/404.html")
 		return
 	}
 
 	reviews, err := a.store.Reviews(id, region(r))
 	if err != nil {
-		log.Println(err.Error())
+		log.Println(err)
 		reviews = []*itunes.Review{}
 	}
 
-	render(w, r, podcastAndReviews{podcast, reviews}, nil, "./templates/base.html", "./templates/podcast.html")
-
+	render(w, r, response{podcast, reviews}, "./templates/podcast.html")
 }
 
-func render(w http.ResponseWriter, r *http.Request, data any, err error, templates ...string) {
-	if err != nil {
-		log.Println(err.Error())
+func render(w http.ResponseWriter, r *http.Request, data any, tmpl string) {
+	var (
+		init sync.Once
+		tpl  *template.Template
+		err  error
+	)
+	type response struct {
+		Data    any
+		Region  string
+		Regions []itunes.Region
 	}
 
-	ts, err := template.ParseFiles(templates...)
+	init.Do(func() {
+		tpl, err = template.ParseFiles("./templates/base.html", tmpl)
+	})
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		log.Println(err)
+		http.Error(w, errorMessage, http.StatusInternalServerError)
 		return
 	}
 
-	if err = ts.Execute(w, &response{
+	if err = tpl.Execute(w, &response{
+		Data:    data,
 		Region:  region(r),
 		Regions: itunes.Regions,
-		Data:    data,
 	}); err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		log.Println(err)
+		http.Error(w, errorMessage, http.StatusInternalServerError)
 		return
 	}
 }
@@ -155,10 +166,4 @@ func region(r *http.Request) string {
 	}
 
 	return cookie.Value
-}
-
-type response struct {
-	Region  string
-	Regions []itunes.Region
-	Data    any
 }
